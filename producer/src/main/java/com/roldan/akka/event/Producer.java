70a09dcfcda5831d36ddfdef9f5ad838cca5b7ec
Jorge Roldán <jorge.roldan@gmail.com>
2019-03-10 11:15:17 +0100

    Create producer module

---
+++ producer/src/main/java/com/roldan/akka/event/Producer.java
@@ -0,0 +1,11 @@
+package com.roldan.akka.event;
+
+public class Producer {
+
+    public static void main(String[] args) {
+//        AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+//        CreateQueueRequest create_request = new CreateQueueRequest(QUEUE_NAME)
+//                .addAttributesEntry("DelaySeconds", "60")
+//                .addAttributesEntry("MessageRetentionPeriod", "86400");
+    }
+}
